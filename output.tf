b2a7945beb6b1977bc310437fc844fddf176dc8e
stoffee <chris@roadkill.org>
2024-03-27 13:19:36 -0700

    output

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "aws_instance_login_information" {
   value = <<INSTANCEIP
 
 Your Rosetta@home AWS instance has been created
-  $ ssh -i ${var.ssh_key_name}.pem ubuntu@${aws_instance.rosettaathome.public_ip}
+  $ ssh -i ${var.ssh_key_name}.pem ubuntu@${aws_instance.rhel7_vault.public_ip}
 
 INSTANCEIP
 }
