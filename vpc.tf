7233cb15c88e26f1c8c8634f3fcedf7d737b6753
stoffee <chris@roadkill.org>
2024-03-27 12:46:06 -0700

    sg updates

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "172.16.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
 }
