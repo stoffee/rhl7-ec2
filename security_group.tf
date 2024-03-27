5fbc2232a2569c81ad6a77b3007aed62580e5fc4
stoffee <chris@roadkill.org>
2024-03-27 12:37:34 -0700

    sg update

--- security_group.tf
+++ security_group.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
@@ -17,7 +17,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_22_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
@@ -25,13 +25,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_22_ipv6" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
