49797d2984762c6140059adbb9deee12875dac52
stoffee <chris@roadkill.org>
2024-03-27 12:21:58 -0700

    adds

--- variables.tf
+++ variables.tf
@@ -1,14 +1,15 @@
-variable "boinc_project_id" {
-  description = "Boinc Project id: boinccmd --lookup_account URL email password https://boinc.berkeley.edu/wiki/Boinccmd_tool"
-}
+#variable "boinc_project_id" {
+#  description = "Boinc Project id: boinccmd --lookup_account URL email password https://boinc.berkeley.edu/wiki/Boinccmd_tool"
+#}
 variable "ssh_key_name" {
   description = "You AWS SSH KeyName"
+  default = "cdunlap-sandbox-aws"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+#variable "aws_region" {}
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.medium"
   description = "Please be careful with cost!!!!"
 }
