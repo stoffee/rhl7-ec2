d94845b89fe6ffdcd8e15911fc9b96b91e2f311d
stoffee <chris@roadkill.org>
2024-03-27 12:38:34 -0700

    sg updates

--- security_group.tf
+++ security_group.tf
@@ -16,22 +16,8 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_22_ipv6" {
-  security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
